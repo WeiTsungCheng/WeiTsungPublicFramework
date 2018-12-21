2c7ae7889478b3677dd18d49f4486478cd0ff317
it-william <william.cheng@kkday.com>
2018-12-21 16:48:20 +0800

    second commit

---
+++ WeiTsungPublicFramework/WeiTsungPublicFramework/Service.swift
@@ -0,0 +1,21 @@
+//
+//  Service.swift
+//  WeiTsungPublicFramework
+//
+//  Created by wei-tsung-cheng on 2018/12/21.
+//  Copyright © 2018 wei-tsung cheng. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+
+    private init() {
+
+    }
+
+    public static func dosomething() -> String {
+        return "Life is long, try your best to do anything"
+    }
+}
+
---
+++ WeiTsungPublicFramework/WeiTsungPublicFramework/WeiTsungPublicFramework.h
@@ -0,0 +1,19 @@
+//
+//  WeiTsungPublicFramework.h
+//  WeiTsungPublicFramework
+//
+//  Created by wei-tsung-cheng on 2018/12/21.
+//  Copyright © 2018 wei-tsung cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WeiTsungPublicFramework.
+FOUNDATION_EXPORT double WeiTsungPublicFrameworkVersionNumber;
+
+//! Project version string for WeiTsungPublicFramework.
+FOUNDATION_EXPORT const unsigned char WeiTsungPublicFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeiTsungPublicFramework/PublicHeader.h>
+
+
